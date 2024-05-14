11e9606907ee9428f464c73c2cd268f86e16f580
bael172 <bael171104@gmail.com>
2024-05-15 01:47:31 +0300

    добавление condition в регистрацию auth.js

--- queries/auth.js
+++ queries/auth.js
@@ -15,75 +15,95 @@ const generateJwt = (id,name,email,phone,role) => {
 
 class AuthController{
     async registration(req, res, next){
-        const {s_name, s_email, s_phone, s_passwd, s_passwdAgain, s_role} = req.body
-        if(!s_email || !s_phone || !s_passwd) {
-            return next(ApiError.badRequest('Введите эл.почту, телефон и придумайте пароль'))
+        const {s_name:name, s_email:email, s_phone:phone, 
+            s_passwd:passwd, s_passwdAgain:passwdAgain, s_role:role} = req.body
+        if(!email || !phone || !passwd) {
+            res.status(401).json({message:'Введите эл.почту, телефон и придумайте пароль'})
+            //return next(ApiError.badRequest('Введите эл.почту, телефон и придумайте пароль'))
+            return
         }
-        if(!s_passwdAgain){
-            return next(ApiError.badRequest('Введите пароль еще раз'))
+        if(!passwdAgain){
+            res.status(403).json({message:'Введите пароль еще раз'})
+            //return next(ApiError.badRequest('Введите пароль еще раз'))
+            return
         }
-        if(s_passwd!==s_passwdAgain){
-            return next(ApiError.badRequest('Пароли не совпадают'))
+        if(passwd!==passwdAgain){
+            res.status(404).json({message:'Пароли не совпадают'})
+            //return next(ApiError.badRequest('Пароли не совпадают'))
+            return
         }
-        const candidate = await User.findOne({
-            where:{
-                [Op.or]:[{email:s_email},{phone:s_phone}]
+        const obj = {email,phone}
+        let condition = []
+        condition = Object.entries(obj).reduce(accum,([key,value])=>{
+            if(value){ //запись в условие значений не являющихся null или undefined
+                accum[key]=value
             }
+            return accum
+        },{}) //используем объект как первичное значение accum
+        const candidate = await User.findOne({
+            where:{[Op.or]:condition}
     })
         if(candidate) {
             return next(ApiError.badRequest('Пользователь с такой почтой или телефоном уже существует'))
         }
-        if(!s_passwdAgain){
+        if(!passwdAgain){
             return next(ApiError.badRequest("Повторно введите ваш пароль"))
         }
-        if(s_passwd == s_passwdAgain){
-            const hashpasswd = await bcrypt.hash(s_passwd,5)
+        if(passwd == passwdAgain){
+            const hashpasswd = await bcrypt.hash(passwd,5)
             const user = await User.create({
-                name:s_name,
-                email:s_email,
-                phone:s_phone,
+                name:name,
+                email:email,
+                phone:phone,
                 passwd:hashpasswd,
-                role:s_role})
+                role:role})
             const token = generateJwt(user.id_user,user.name,user.email,user.phone,user.role)
-            return res.json({token})
+            return res.status(200).json({token})
         }
-        else return next(ApiError.badRequest('Пароли не совпадают'))
+        else res.status(200).json({message:"Пароли не совпадают"})
+        //return next(ApiError.badRequest('Пароли не совпадают'))
         
     }
 
     async login(req,res,next){
-        const {s_email,s_phone,s_passwd} = req.body
-        if(!s_email && !s_passwd || !s_phone && !s_passwd){
-                res.status(401).json({message:'Введите эл.почту/телефон и пароль'})            
-                //return next(ApiError.badRequest('Введите эл.почту / телефон и пароль'))
-                return
+        const {s_email:email,s_phone:phone,s_passwd:passwd} = req.body
+        if(!(email||phone)){
+            res.status(401).json({message:'Введите эл.почту/телефон'})            
+            //return next(ApiError.badRequest('Введите эл.почту / телефон и пароль'))
+            return
             }
-        let condition = {}
-        Object.entries(req.body).forEach(function([key,value]){
-            if(value!==undefined) 
-            switch(key){
-                case ('s_email'): key='email'; break
-                case ('s_phone'): key='phone'; break
-                case ('s_passwd'): key='passwd'; break
+        if(!passwd){
+            res.status(402).json({message:'Введите пароль'})            
+            //return next(ApiError.badRequest('Введите эл.почту / телефон и пароль'))
+            return
+        }
+        const obj={email,phone} //объект для динамического условия из-за возможности не вводить почту или телефон
+        let condition = []
+        condition = Object.entries(obj).reduce((accum,[key,value])=>{ //запись в accum пар [key,value]
+            if(value) { //запись значений не являющихся undefined или null
+                accum[key]=value
             }
-            condition[key]=value //сохранение в условие значений не являющихся undefined
-        })
+            return accum
+        },{}) //используем объект как первичное значение accum
         console.log(condition)
 
         const user = await User.findOne({
             where:{[Op.or]:condition}
         })
         if(!user){
-            res.status(402).json({message:'Введен неверный email/телефон или нет учётной записи'})
+            res.status(403).json({message:'Введен неверный email/телефон или нет учётной записи'})
             //return next(ApiError.internal('Введен неверный email/телефон или нет учётной записи'))
+            return
         }
 
         //Сравнение незашифрованного пароля passwd с зашифрованным user.passwd (passwd:hashpasswd)
-        let comparePassword = bcrypt.compareSync(s_passwd, user.passwd)
+        let comparePassword = bcrypt.compareSync(passwd, user.passwd)
         if(!comparePassword){ //если пароли не совпадают
-            res.status(403).json({message:'Указан неверный пароль'})
+            res.status(404).json({message:'Указан неверный пароль'})
             //return next(ApiError.internal("Указан неверный пароль"))
+            return
         }
+        
         const token = generateJwt(user.id_user,user.name, user.email, user.phone, user.role)
         return res.status(200).json({token})
         //res.redirect('/path/path')
